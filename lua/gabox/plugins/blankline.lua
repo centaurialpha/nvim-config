3f15ff7d8ee48d7a2994d95cdda3dcec7447e30b
Gabriel Acosta <acostadariogabriel@gmail.com>
2023-11-21 03:05:18 -0300

    change color blankline

--- lua/gabox/plugins/blankline.lua
+++ lua/gabox/plugins/blankline.lua
@@ -1,25 +1,32 @@
+vim.api.nvim_set_hl(0, "IndentBlanklineIndent", { fg = "#2c2c39" })
+
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  opts = {
-    scope = { enabled = false },
-    indent = {
-      char = "│",
-      tab_char = "│",
-    },
-    exclude = {
-      filetypes = {
-        "help",
-        "terminal",
-        "lazy",
-        "lspinfo",
-        "TelescopePrompt",
-        "TelescopeResults",
-        "mason",
-        "nvdash",
-        "nvcheatsheet",
-        "",
+  config = function()
+    require("ibl").setup({
+      scope = { enabled = false },
+      indent = {
+        highlight = {
+          "IndentBlanklineIndent",
+        },
+        char = "│",
+        tab_char = "│",
       },
-    },
-  },
+      exclude = {
+        filetypes = {
+          "help",
+          "terminal",
+          "lazy",
+          "lspinfo",
+          "TelescopePrompt",
+          "TelescopeResults",
+          "mason",
+          "nvdash",
+          "nvcheatsheet",
+          "",
+        },
+      },
+    })
+  end,
 }
