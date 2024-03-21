739ddc9b7b83a680de6c950a1548aa2474549bc0
Gabriel Acosta <acostadariogabriel@gmail.com>
2024-03-20 22:32:56 -0300

    fix status line

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -94,9 +94,40 @@ Statusline.lsp = function()
 end
 
 Statusline.git = function()
-  local head = vim.b.gitsigns_head or "-"
-  local signs = vim.b.gitsigns_status or ""
-  return string.format("  %s %s ", head, signs)
+  local colors = false
+  if not colors then
+    local git_info = vim.b.gitsigns_status
+    if not git_info then
+      return ""
+    end
+    return git_info
+  end
+
+  local git_info = vim.b.gitsigns_status_dict
+  if not git_info then
+    return ""
+  end
+
+  local added = git_info.added and ("%#GitSignsAdd#+" .. git_info.added .. "%* ") or ""
+  local changed = git_info.changed and ("%#GitSignsChange#~" .. git_info.changed .. "%* ") or ""
+  local removed = git_info.removed and ("%#GitSignsDelete#-" .. git_info.removed .. "%* ") or ""
+  if git_info.added == 0 then
+    added = ""
+  end
+  if git_info.changed == 0 then
+    changed = ""
+  end
+  if git_info.removed == 0 then
+    removed = ""
+  end
+  return table.concat({
+    " ",
+    added,
+    changed,
+    removed,
+    " ",
+    " %*",
+  })
 end
 
 Statusline.location = function()
