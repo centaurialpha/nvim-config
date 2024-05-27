ff56f7d304ba1628316146d0f68b92014f4733e6
Gabriel Acosta <gabriel.acosta@satellogic.com>
2024-05-27 18:52:04 -0300

    add oil as file explorer

--- lua/gabox/plugins/nvim-tree.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-tree/nvim-tree.lua",
-  config = function()
-    local nvimtree = require("nvim-tree")
-    nvimtree.setup({})
-    vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>")
-  end,
-}
