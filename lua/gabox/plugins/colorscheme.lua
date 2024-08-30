3a935301bfc7850e1c289e3dcd1b52f545c10af1
Gabriel Acosta <acostadariogabriel@gmail.com>
2024-08-30 02:44:08 -0300

    clean colorscheme

--- lua/gabox/plugins/colorscheme.lua
+++ lua/gabox/plugins/colorscheme.lua
@@ -1,24 +1,10 @@
 return {
-  "projekt0n/github-nvim-theme",
+  "zenbones-theme/zenbones.nvim",
   lazy = false,
+  dependencies = "rktjmp/lush.nvim",
   priority = 1000,
   config = function()
-    local groups = {
-      all = {
-        StatusLine = { bg = "bg0" },
-        Whitespace = { fg = "#444444" },
-      },
-    }
-    local specs = {
-      all = {
-        syntax = {
-          -- type = "red",
-        },
-        bg1 = "#000000"
-      },
-    }
-    require("github-theme").setup({ groups = groups, specs = specs })
-
-    vim.cmd("colorscheme github_dark_colorblind")
-  end,
+    vim.g.zenbones = { darkness = "stark" }
+    vim.cmd("colorscheme zenbones")
+  end
 }
