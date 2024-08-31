6fc728fe9e3da06f387d8677269f4a2262de8adc
Gabriel Acosta <acostadariogabriel@gmail.com>
2024-08-31 19:10:02 -0300

    fix colorscheme context

--- lua/gabox/plugins/colorscheme.lua
+++ lua/gabox/plugins/colorscheme.lua
@@ -5,6 +5,21 @@ return {
   priority = 1000,
   config = function()
     vim.g.zenbones = { darkness = "stark" }
+
+    vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#171717" })
     vim.cmd("colorscheme zenbones")
+
+    -- local lush = require "lush"
+    -- local base = require "zenbones"
+
+    -- -- Create some specs
+    -- local specs = lush.parse(function()
+    --   return {
+    --     Normal { bg = "#171210" },
+    --     CursorLine { bg = "#171717"}
+    --   }
+    -- end)
+    -- -- Apply specs using lush tool-chain
+    -- lush.apply(lush.compile(specs))
   end
 }
