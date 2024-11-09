56c37a7d3add35f6548eb48760d3d01226165cab
Gabriel Acosta <acostadariogabriel@gmail.com>
2024-11-08 21:46:47 -0300

    back to github colorscheme

--- lua/gabox/plugins/colorscheme.lua
+++ lua/gabox/plugins/colorscheme.lua
@@ -1,25 +1,17 @@
 return {
-  "zenbones-theme/zenbones.nvim",
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
   lazy = false,
-  dependencies = "rktjmp/lush.nvim",
   priority = 1000,
   config = function()
-    vim.g.zenbones = { darkness = "stark" }
-
-    vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#171717" })
-    vim.cmd("colorscheme zenbones")
-
-    -- local lush = require "lush"
-    -- local base = require "zenbones"
-
-    -- -- Create some specs
-    -- local specs = lush.parse(function()
-    --   return {
-    --     Normal { bg = "#171210" },
-    --     CursorLine { bg = "#171717"}
-    --   }
-    -- end)
-    -- -- Apply specs using lush tool-chain
-    -- lush.apply(lush.compile(specs))
+    require("github-theme").setup({
+      groups = {
+        all = {
+          StatusLine = { bg = "#000000" },
+          CursorLine = { bg = "#191919" },
+        }
+      }
+    })
+    vim.cmd("colorscheme github_dark_default")
   end
 }
