28c10c355fd9b2a786b26f4dc6f41f14ed8240d3
Gabriel Acosta <acostadariogabriel@gmail.com>
2026-04-11 02:56:16 -0300

    update

--- lua/gabox/plugins/treesitter.lua
+++ lua/gabox/plugins/treesitter.lua
@@ -1,32 +1,83 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua",
-        "astro",
-        "tsx",
-        "typescript",
-        "html",
-        "python",
-        "c",
-        "cpp",
-        "yaml",
-        "json",
-        "dockerfile",
-        "bash",
-        "markdown",
-        "rust",
-        "toml",
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      auto_install = true,
+    require("nvim-treesitter").setup()
+
+    -- Find the name of parsers with:
+    --  := require("nvim-treesitter").get_available()
+    -- (LaTeX clashes with Vimtex)
+    local languages = {
+      "bash",
+      "c",
+      "cpp",
+      "fish",
+      "html",
+      "java",
+      "javascript",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "sql",
+      "vimscript",
+      "vimdoc",
+      "dockerfile",
+      "rust",
+      "toml",
+      "yaml",
+    }
+
+    local filetypes = {}
+    for _, lang in ipairs(languages) do
+      for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
+        table.insert(filetypes, ft)
+      end
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = filetypes,
+      callback = function()
+        vim.treesitter.start()
+        vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo[0][0].foldmethod = "expr"
+        -- Indentation is Experimental
+        -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
+-- return {
+--   "nvim-treesitter/nvim-treesitter",
+--   build = ":TSUpdate",
+--   config = function()
+--   branch = "main",
+--     require("nvim-treesitter.configs").setup({
+--       ensure_installed = {
+--         "lua",
+--         "astro",
+--         "tsx",
+--         "typescript",
+--         "html",
+--         "python",
+--         "c",
+--         "cpp",
+--         "yaml",
+--         "json",
+--         "dockerfile",
+--         "bash",
+--         "markdown",
+--         "rust",
+--         "toml",
+--       },
+--       highlight = {
+--         enable = true,
+--       },
+--       indent = {
+--         enable = true,
+--       },
+--       auto_install = true,
+--     })
+--   end,
+-- }
