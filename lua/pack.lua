1c663c92eb88a7b7686875241cc08dab888689d6
Gabriel Acosta <acostadariogabriel@gmail.com>
2026-06-08 04:13:07 -0300

    refactor: update configs for v0.12

--- lua/pack.lua
+++ lua/pack.lua
@@ -1,13 +1,15 @@
-vim.pack.add({
-    "https://github.com/bluz71/vim-moonfly-colors",
-    "https://github.com/stevearc/oil.nvim",
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main" },
-    "https://github.com/lewis6991/gitsigns.nvim",
-    "https://github.com/ibhagwan/fzf-lua",
-    "https://github.com/NMAC427/guess-indent.nvim",
-    "https://github.com/folke/todo-comments.nvim",
-    "https://github.com/nvim-mini/mini.nvim",
-    "https://github.com/sschleemilch/slimline.nvim",
-})
+local function gh(repo) return "https://github.com/" .. repo end
+
+vim.pack.add {
+    gh "bluz71/vim-moonfly-colors",
+    gh "stevearc/oil.nvim",
+    gh "lewis6991/gitsigns.nvim",
+    gh "ibhagwan/fzf-lua",
+    gh "NMAC427/guess-indent.nvim",
+    gh "folke/todo-comments.nvim",
+    gh "nvim-mini/mini.nvim",
+    gh "sschleemilch/slimline.nvim",
+    { src = gh "nvim-treesitter/nvim-treesitter", version = "main" },
+}
 
 require("guess-indent").setup {}
